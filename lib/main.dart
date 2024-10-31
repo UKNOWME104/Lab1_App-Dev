021768bc79be0cb00b881668ec2600a289edc6e3
UKNOWME104 <shaikhziasaeed08@gmail.com>
2024-10-31 21:28:00 +0500

    not done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:assignment1/ProductBloc.dart';
+import 'package:assignment1/calculator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_shimmer/flutter_shimmer.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -9,129 +8,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'My App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: BlocProvider(
-            create: (context) => ProductBloc()..add(FetchProducts()),
-            child: ProductScreen()
-        )
-      //home: ProductsList(),
-    );
-  }
-}
-
-
-
-
-class ProductScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Products')),
-      body: BlocBuilder<ProductBloc, ProductState>(
-        builder: (context, state) {
-          if (state is ProductLoading) {
-            // Show a skeleton loading list when data is loading
-            return const SkeletonList();
-          } else if (state is ProductLoaded) {
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView.builder(
-                itemCount: state.products.length,
-                itemBuilder: (context, index) {
-                  final product = state.products[index];
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    elevation: 4,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: Colors.grey,
-                            child: Image.network(
-                              product.thumbnail,
-                              height: 80,
-                              width: 80,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  product.title,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  product.description,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                    color: Colors.grey[600],
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                SizedBox(height: 10),
-                                Text(
-                                  '\$${product.price.toStringAsFixed(2)}',
-                                  style: TextStyle(
-                                    color: Colors.blueAccent,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            );
-          } else if (state is ProductError) {
-            return Center(child: Text(state.error));
-          }
-          return Center(child: Text('Press button to fetch products'));
-        },
-      ),
-    );
-  }
-}
-
-class SkeletonList extends StatelessWidget {
-  const SkeletonList({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) => ListTileShimmer(
-        isDisabledAvatar: false,
-        isPurplishMode: true,
-        isRectBox: true,
-        colors: [Colors.grey[300]!, Colors.grey[100]!],
-        hasBottomBox: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+          useMaterial3: false,
+          fontFamily: 'Helvetica',
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(color: Colors.white),
+            bodyMedium: TextStyle(color: Colors.white),
+            bodySmall: TextStyle(color: Colors.white),
+          ),
+          scaffoldBackgroundColor: Colors.black),
+      home: Calculator(),
     );
   }
 }
