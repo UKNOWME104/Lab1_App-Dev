489ba12dc865b5538079db5d478da77016a77783
UKNOWME104 <shaikhziasaeed08@gmail.com>
2024-11-05 09:33:53 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,5 @@
+import 'package:assignment1/app.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'calculator.dart';
-import 'calculator_provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => CalculatorProvider()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Calculator',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-          scaffoldBackgroundColor: Colors.black,
-        ),
-        home: Calculator(),
-      ),
-    );
-  }
-}
+void main() => runApp(const App());
